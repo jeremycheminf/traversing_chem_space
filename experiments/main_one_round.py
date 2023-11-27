c636502f58a210c01f8d8e49e46a989668440a04
jeremycheminf <j10b84@hotmail.com>
2023-11-27 20:44:15 +0000

    try np array to pandas

--- experiments/main_one_round.py
+++ experiments/main_one_round.py
@@ -92,8 +92,8 @@ if __name__ == '__main__':
         results['batch_size'] = experiment['batch_size']
         results['seed'] = experiment['seed']
         results['bias'] = experiment['bias']
-        col_names = ['col' + str(i) for i in np.arange(compounds.shape[0]) + 1]
-        pick_df = pd.DataFrame(data=compounds.T, columns=col_names)
+        #col_names = ['col' + str(i) for i in np.arange(compounds.shape[0]) + 1]
+        pick_df = pd.DataFrame(data=compounds)
         pick_df.to_csv(output_file = args.out, mode='a', index=False, header=False if os.path.isfile(output_file) else True)
 
         results.to_csv(LOG_FILE, mode='a', index=False, header=False if os.path.isfile(LOG_FILE) else True)
